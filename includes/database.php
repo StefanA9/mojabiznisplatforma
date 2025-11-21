2ae6e3133db2e7e420a2cab9777277b966c38362
StefanA9 <stefanagoc@gmail.com>
2025-11-21 14:20:58 +0100

    dodavanje baze

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'sup25_sa';
-$username = 'root';
-$password = '';
+$host = 'sql200.infinityfree.com';
+$dbname = 'epiz_31121671_agocs';
+$username = 'epiz_31121671';
+$password = 'ndvWKGSBwCsWZP';
+$port = 3306;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
